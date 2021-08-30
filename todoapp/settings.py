46a21ee4a4acb4a558509f736c12f7d40e7c302c
kash <vatsakash168@gmail.com>
2021-08-30 15:43:15 +0530

    'version'

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1','fodoapp.herokuapp.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.auth.backends.ModelBackend',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -76,18 +75,24 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd4b2fjovmu65v9',
-        'USER':'rmrjjsaokeqwcy',
-        'PASSWORD':'6a89beb5088cd4cced4110b71b550e9391cd80d314f0981e041718c864ed174d',
-        'HOST':'ec2-34-228-100-83.compute-1.amazonaws.com',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd4b2fjovmu65v9',
+#         'USER':'rmrjjsaokeqwcy',
+#         'PASSWORD':'6a89beb5088cd4cced4110b71b550e9391cd80d314f0981e041718c864ed174d',
+#         'HOST':'ec2-34-228-100-83.compute-1.amazonaws.com',
+#         'PORT':'5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
